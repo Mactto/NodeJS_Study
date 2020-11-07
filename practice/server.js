d648d8723139000038a4aef469ecc7f2e7b26d59
mactto <shc9928@gmail.com>
2020-11-08 00:16:43 +0900

    updates

---
+++ practice/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require('fs');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(8080, function() {
+    console.log('server started');
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '@#@$SMYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+var router = require('./router/main')(app, fs);
