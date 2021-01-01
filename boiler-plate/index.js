b397bca310aaf3c965a132d66ccdb7a552043e04
Mactto <shc9928@gmail.com>
2021-01-01 14:17:29 +0900

    config

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
 const mongoose = require('mongoose');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://mactto:akreh0707@boilerplate.ch2pk.mongodb.net/test?retryWrites=true&w=majority', {
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 시 필요한 정보들을 client 에서 가져오면 
+    // DB에 삽입
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.statux(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => console.log('server on'));
