78e01b5a4adf652b76ed6eaaddc550aad29f284b
Mactto <shc9928@gmail.com>
2021-01-07 15:17:18 +0900

    updates

--- practice/Login_Example/server/index.js
+++ practice/Login_Example/server/index.js
@@ -3,6 +3,18 @@ const app = express();
 const port = 3000;
 const router = require('./router/route');
 
+const mongoose = require('mongoose');
+const config = require("./config/key");
+
+const connect = mongoose.connect(config.mongoURI, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('Mongo DB Connected...'))
+.catch(err => console.log(err))
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
--- practice/Login_Example/server/router/user/user.js
+++ practice/Login_Example/server/router/user/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {User} = require('../../models/User');
+const mongoose = require('mongoose');
+
 
 router.post('/register', (req, res) => {
+    
 })
 
 module.exports = router;
