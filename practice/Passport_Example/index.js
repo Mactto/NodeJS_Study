46fc39a7bbbe2f44e1de07f5b2a23a4741f1ffc8
Mactto <shc9928@gmail.com>
2021-01-14 16:13:15 +0900

    passport_compelete

--- practice/Passport_Example/index.js
+++ practice/Passport_Example/index.js
@@ -1,31 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const passportConfig = require('./passport');
 const session = require('express-session');
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'Mac34tto87~!',
-    database: 'passport'
-})
-
-connection.connect();
+const port = 3000;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(session({
-    secret: 'asdfkj!',
-    resave: false,
-    saveUninitialized: true
-}));
+
+app.use(session({secret: 'asdfkj~!', resave: false, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -34,51 +20,31 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.render('join');
+    res.render('login');
 })
 
-app.post('/login', 
+app.post('/login',
     passport.authenticate('local', {
         successRedirect: '/',
         failureRedirect: '/login'
-}));
-
-passport.serializeUser(function(user, done) {
-    done(null, user.email);
-});
-
-passport.deserializeUser(function(email, done) {
-    var sql = "select * from user where email='" + email + "';";
-    connection.query(sql, (err, rows, fields) => {
-        console.log(rows[0]);
-        done(err, rows[0]);
     })
-})
+);
 
-passport.use(new LocalStrategy(
-    {
-        usernameField: 'email',
-        passwordField: 'pwd'
-    },
-    function(username, password, done) {
-        const pwd = password;
-        var sql = "select * from user where email='" + username + "';";
-        connection.query(sql, (err, rows, fields) => {
-            if(err) return done(err);
-            if (rows[0] == null) {
-                console.log('no user');
-                return done(null, false, {message: '해당 이메일의 유저가 없습니다.'});
-            } 
-            if (rows[0].password !== pwd) {
-                console.log('password wrong');
-                return done(null, false, {message: '비밀번호가 다릅니다'});
-            }
-            console.log('complete');
-            return done(null, rows[0]);
-        })
+app.get('/facebook',
+    passport.authenticate('facebook', {
+        authType: 'rerequest',
+        scope: ['public_profile', 'email']
+    })
+);
+
+app.get('/facebook/callback',
+    passport.authenticate('facebook', {
+        failureRedirect: '/'
+    }), (req, res) => {
+        res.redirect('/');
     }
-))
+)
 
 app.listen(port, () => {
-    console.log('Server is Working');
+    console.log('Server is working');
 })
--- practice/Passport_Example/passport.js
+++ practice/Passport_Example/passport.js
@@ -1,30 +1,47 @@
 const passport = require('passport');
-const LocalStrategy = require('passport.local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const FacebookStrategy = require('passport-facebook');
 const db = require('./database');
 
-db.connect();
+passport.serializeUser(function(user, done) {
+    console.log('serialize');
+    return done(null, user.email);
+})
 
-module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user);
+passport.deserializeUser(function(email, done) {
+    var sql = "select * from user where email='" + email + "';";
+    db.query(sql, (err, rows, feilds) => {
+        console.log('deserialize ', rows[0]);
+        return done(err, rows[0]);
     })
+})
 
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    })
-
-    passport.use('local', LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password',
-        session: true,
-        passReqToCallback: true,
-    }, (req, email, password, done) => {
-        var sql = "select email from user where email='" + email + "';";
-        db.query(sql, function(err, rows, fields) {
-            if (err) console.log(err);
-            console.log('rows', rows);
-            console.log('fields', fields);
-            connection.end();
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        var sql = "select * from user where email='" + username + "';";
+        var pwd = password;
+        db.query(sql, (err, rows, fields) => {
+            if (err) { return done(err) };
+            if (rows[0].email === null) {
+                console.log('not user');
+                return done(null, false, {message: '유저가 존재하지 않습니다.'});
+            }
+            if (rows[0].password !== pwd) {
+                console.log('password wrong');
+                return done(null, false, {message: '비밀번호가 틀렸습니다.'});
+            }
+            return done(null, rows[0]);
         })
-    }))
-}
+    }
+))
+
+passport.use(new FacebookStrategy({
+    clientID: "아이디",
+    clientSecret: '시크릿',
+    callbackURL: "주소",
+    passReqToCallback: true,
+}, (req, acessToken, refreshToken, profile, done) => {
+    console.log(profile);
+}))
+
+module.exports = passport;
